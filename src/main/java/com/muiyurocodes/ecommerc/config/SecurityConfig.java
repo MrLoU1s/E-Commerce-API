c044463ed37f92cfd5e3b9ff004f9c5aa3abc622
MrLoU1s <muiyurolouis@gmail.com>
2025-09-21 12:45:51 +0000

    Phase 3: Spring Security Configuration: configures the security rules for our API and create the filters that will perform the authentication and authorization login. UserLoginDTO - structure of request body that client sends when they want to login. LoginResponseDTO - contains accessTOken and UserResponseDTO object. SecurityConfig - Add @EnableWebSecurity, Defines AuthenticationManager bean, Define SecurityFilterChain bean which; diasables CSRF protection because we are using stateless session management, configure endpoints.

--- src/main/java/com/muiyurocodes/ecommerc/config/SecurityConfig.java
+++ src/main/java/com/muiyurocodes/ecommerc/config/SecurityConfig.java
@@ -1,15 +1,59 @@
 package com.muiyurocodes.ecommerc.config;
 
+import com.muiyurocodes.ecommerc.security.JwtAuthenticationFilter;
+import com.muiyurocodes.ecommerc.security.JwtAuthorizationFilter;
+import com.muiyurocodes.ecommerc.security.JwtTokenProvider;
+import com.muiyurocodes.ecommerc.service.SessionService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final SessionService sessionService;
+    private final ModelMapper modelMapper;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenProvider, sessionService, modelMapper);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/api/auth/login");
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/users/register", "/api/auth/login", "/api/auth/refresh").permitAll()
+                        .requestMatchers("/api/products/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilter(jwtAuthenticationFilter)
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
